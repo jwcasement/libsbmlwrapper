9bbbd3af4d1070bcfde2fe8a60b09a3c2b750757
John Casement <j.w.casement@ncl.ac.uk>
2013-08-10 20:55:05 +0100

    Delete zzz.R

--- package/R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(...) {
-  #create a global variable to store setter and getter functions for defaults
-  assign("pkg_globals", new.env(), envir = globalenv())
-  assign("defs", set_and_get_defaults(), pkg_globals)
-  assign("model_level", 0, pkg_globals)
-  assign("model_version", 0, pkg_globals)
-}
