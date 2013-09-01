b236a52fc94f60868654e35e058545f214ab2de9
John Casement <j.w.casement@ncl.ac.uk>
2013-09-01 09:07:31 +0100

    Update read.R

--- package/R/read.R
+++ package/R/read.R
@@ -5,7 +5,7 @@
 #' @title Read an SBML file
 #' @param filename a character string representing an SBML file 
 #' @docType methods
-#' @rdname read_SBML
+#' @note \code{readSBML} is an alias for \code{readSBMLFromFile}.
 #' @export
 readSBML = function(filename) {
   
@@ -14,33 +14,31 @@ readSBML = function(filename) {
   level = SBase_getLevel(doc)
   vers = SBase_getVersion(doc)
   
-  # get level and version of currently loaded model
-  current_level = get("model_level", pkg_globals)
-  current_vers = get("model_version", pkg_globals)
+  # get level and version of currently loaded document
+  current_level = get("doc_level", pkg_globals)
+  current_vers = get("doc_version", pkg_globals)
   
-  # only generate defaults if level or version of model being loaded
-  # are different to the existing model
+  # only generate defaults if level or version of document being loaded
+  # are different to the existing document
   if((level != current_level) || (vers != current_vers)) {
     # set defaults according to document level and version
     get("defs", pkg_globals)$set_Defaults(level, vers)
     # update level and version variables in global
-    assign("model_level", level, pkg_globals)
-    assign("model_version", vers, pkg_globals)    
+    assign("doc_level", level, pkg_globals)
+    assign("doc_version", vers, pkg_globals)    
   }
   
   return(doc)
 }
 
+#' @rdname readSBML
+#' @export
+readSBMLFromFile = function(filename) {
+  readSBML(filename)
+}
 
-
-#' Read an SBML document from an xml string
-#' 
-#' Reads SBML document, sets defaults, and returns a _p_SBMLDocument object
-#' 
-#' @title Read an SBML document from an xml string
+#' @rdname readSBML
 #' @param xml string containing an SBML file 
-#' @docType methods
-#' @rdname read_SBML_from_string
 #' @export
 readSBMLFromString = function(xml) {
   
@@ -49,18 +47,18 @@ readSBMLFromString = function(xml) {
   level = SBase_getLevel(doc)
   vers = SBase_getVersion(doc)
   
-  # get level and version of currently loaded model
-  current_level = get("model_level", pkg_globals)
-  current_vers = get("model_version", pkg_globals)
+  # get level and version of currently loaded document
+  current_level = get("doc_level", pkg_globals)
+  current_vers = get("doc_version", pkg_globals)
   
-  # only generate defaults if level or version of model being loaded
-  # are different to the existing model
+  # only generate defaults if level or version of document being loaded
+  # are different to the existing document
   if((level != current_level) || (vers != current_vers)) {
     # set defaults according to document level and version
     get("defs", pkg_globals)$set_Defaults(level, vers)
     # update level and version variables in global
-    assign("model_level", level, pkg_globals)
-    assign("model_version", vers, pkg_globals)    
+    assign("doc_level", level, pkg_globals)
+    assign("doc_version", vers, pkg_globals)    
   }
   
   return(doc)
