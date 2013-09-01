993dc0aa8bd86b256159b925dd484dba9d0df676
John Casement <j.w.casement@ncl.ac.uk>
2013-09-01 08:57:57 +0100

    Update add.R

--- package/R/add.R
+++ package/R/add.R
@@ -1,412 +1,173 @@
 #' A generic for createFunctionDefinition
 #' 
-#' @export
 #' @docType methods
 #' @rdname createFunctionDefinition-methods
 #' @param obj a_p_Model object
 #' @param i a vector of integers indicating objects to be copied or created
+#' @exportMethod createFunctionDefinition
+#' @note Where a new _p_* object is created, the SBML Level and Version of
+#' the _p_Model object, (and any SBML package namespaces), are used to initialise
+#' the attributes of the new _p_FunctionDefinition object to 'typical' defaults
 setGeneric("createFunctionDefinition",
-           function(obj, i)
-             standardGeneric("createFunctionDefinition")
+           function(obj, i) standardGeneric("createFunctionDefinition")
 )
 
-
-
-#' A generic for createUnitDefinition
-#' 
-#' @export
-#' @docType methods
-#' @rdname createUnitDefinition-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createUnitDefinition",
-           function(obj, i)
-             standardGeneric("createUnitDefinition")
-)
-
-
-
-#' A generic for createCompartmentType
-#' 
-#' @export
-#' @docType methods
-#' @rdname createCompartmentType-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createCompartmentType",
-           function(obj, i)
-             standardGeneric("createCompartmentType")
-)
-
-
-
-#' A generic for createSpeciesType
-#' 
-#' @export
-#' @docType methods
-#' @rdname createSpeciesType-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createSpeciesType",
-           function(obj, i)
-             standardGeneric("createSpeciesType")
-)
-
-
-
-#' A generic for createCompartment
-#' 
-#' @export
-#' @docType methods
-#' @rdname createCompartment-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createCompartment",
-           function(obj, i)
-             standardGeneric("createCompartment")
-)
-
-
-
-#' A generic for createSpecies
-#' 
-#' @export
-#' @docType methods
-#' @rdname createSpecies-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createSpecies",
-           function(obj, i)
-             standardGeneric("createSpecies")
-)
-
-
-
-#' A generic for createParameter
-#' 
-#' @export
-#' @docType methods
-#' @rdname createParameter-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createParameter",
-           function(obj, i)
-             standardGeneric("createParameter")
-)
-
-
-
-#' A generic for createInitialAssignment
-#' 
-#' @export
-#' @docType methods
-#' @rdname createInitialAssignment-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createInitialAssignment",
-           function(obj, i)
-             standardGeneric("createInitialAssignment")
-)
-
-
-
-#' A generic for createConstraint
-#' 
-#' @export
-#' @docType methods
-#' @rdname createConstraint-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createConstraint",
-           function(obj, i)
-             standardGeneric("createConstraint")
-)
-
-
-
-#' A generic for createReaction
-#' 
-#' @export
-#' @docType methods
-#' @rdname createReaction-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createReaction",
-           function(obj, i)
-             standardGeneric("createReaction")
-)
-
-
-
-#' A generic for createEvent
-#' 
-#' @export
-#' @docType methods
-#' @rdname createEvent-methods
-#' @param obj a_p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-setGeneric("createEvent",
-           function(obj, i)
-             standardGeneric("createEvent")
-)
-
-
-
-#' Function for adding a _p_FunctionDefinition object to an SBML model
-#' 
-#' Creates or copies a _p_FunctionDefinition object and adds it to the _p_Model object
-#' 
-#' @title Add _p_FunctionDefinition object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
 #' @rdname createFunctionDefinition-methods
-#' @export
+#' @exportMethod createUnitDefinition
+setGeneric("createUnitDefinition",
+           function(obj, i) standardGeneric("createUnitDefinition")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createCompartmentType
+setGeneric("createCompartmentType",
+           function(obj, i) standardGeneric("createCompartmentType")
+)
+
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createSpeciesType
+setGeneric("createSpeciesType",
+           function(obj, i) standardGeneric("createSpeciesType")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createCompartment
+setGeneric("createCompartment",
+           function(obj, i) standardGeneric("createCompartment")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createSpecies
+setGeneric("createSpecies",
+           function(obj, i) standardGeneric("createSpecies")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createParameter
+setGeneric("createParameter",
+           function(obj, i) standardGeneric("createParameter")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createInitialAssignment
+setGeneric("createInitialAssignment",
+           function(obj, i) standardGeneric("createInitialAssignment")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createConstraint
+setGeneric("createConstraint",
+           function(obj, i) standardGeneric("createConstraint")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createReaction
+setGeneric("createReaction",
+           function(obj, i) standardGeneric("createReaction")
+)
+
+#' @rdname createFunctionDefinition-methods
+#' @exportMethod createEvent
+setGeneric("createEvent",
+           function(obj, i) standardGeneric("createEvent")
+)
+
+#################################################################
+#Methods for above generics
+#################################################################
+
+#' @rdname createFunctionDefinition-methods
 #' @aliases createFunctionDefinition,_p_Model,ANY-method
 #' @aliases createFunctionDefinition,_p_Model-method 
 #' @name createFunctionDefinition
-#' @note Where a new _p_FunctionDefinition object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_FunctionDefinition object to 'typical' defaults
 setMethod("createFunctionDefinition", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("FunctionDefinition", obj, i)
 )
 
 
-
-#' Function for adding a _p_UnitDefinition object to an SBML model
-#' 
-#' Creates or copies a _p_UnitDefinition object and adds it to the _p_Model object
-#' 
-#' @title Add _p_UnitDefinition object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createUnitDefinition-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createUnitDefinition,_p_Model,ANY-method
 #' @aliases createUnitDefinition,_p_Model-method
-#' @name createUnitDefinition
-#' @note Where a new _p_UnitDefinition object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_UnitDefinition object to 'typical' defaults
 setMethod("createUnitDefinition", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("UnitDefinition", obj, i)
 )
 
-
-
-#' Function for adding a _p_CompartmentType object to an SBML model
-#' 
-#' Creates or copies a _p_CompartmentType object and adds it to the _p_Model object
-#' 
-#' @title Add _p_CompartmentType object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createCompartmentType-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createCompartmentType,_p_Model,ANY-method
 #' @aliases createCompartmentType,_p_Model-method
-#' @name createCompartmentType
-#' @note Where a new _p_CompartmentType object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_CompartmentType object to 'typical' defaults
 setMethod("createCompartmentType", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("CompartmentType", obj, i)
 )
 
-
-
-#' Function for adding a _p_SpeciesType object to an SBML model
-#' 
-#' Creates or copies a _p_SpeciesType object and adds it to the _p_Model object
-#' 
-#' @title Add _p_SpeciesType object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createSpeciesType-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createSpeciesType,_p_Model,ANY-method
 #' @aliases createSpeciesType,_p_Model-method
-#' @name createSpeciesType
-#' @note Where a new _p_SpeciesType object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_SpeciesType object to 'typical' defaults
 setMethod("createSpeciesType", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("SpeciesType", obj, i)
 )
-
-
-
-#' Function for adding a _p_Compartment object to an SBML model
-#' 
-#' Creates or copies a _p_Compartment object and adds it to the _p_Model object
-#' 
-#' @title Add _p_Compartment object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createCompartment-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createCompartment,_p_Model,ANY-method
 #' @aliases createCompartment,_p_Model-method
-#' @name createCompartment
-#' @note Where a new _p_Compartment object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_Compartment object to 'typical' defaults
 setMethod("createCompartment", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("Compartment", obj, i)
 )
 
-
-
-#' Function for adding a _p_Species object to an SBML model
-#' 
-#' Creates or copies a _p_Species object and adds it to the _p_Model object
-#' 
-#' @title Add _p_Species object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createSpecies-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createSpecies,_p_Model,ANY-method
 #' @aliases createSpecies,_p_Model-method
-#' @name createSpecies
-#' @note Where a new _p_Species object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_Species object to 'typical' defaults
 setMethod("createSpecies", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("Species", obj, i)
 )
 
-
-
-#' Function for adding a _p_Parameter object to an SBML model
-#' 
-#' Creates or copies a _p_Parameter object and adds it to the _p_Model object
-#' 
-#' @title Add _p_Parameter object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createParameter-methods
-#' @export
+#' @rdname createFunctionDefinition-methods
 #' @aliases createParameter,_p_Model,ANY-method
 #' @aliases createParameter,_p_Model-method
-#' @name createParameter
-#' @note Where a new _p_Parameter object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_Parameter object to 'typical' defaults
 setMethod("createParameter", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("Parameter", obj, i)
 )
 
-
-
-#' Function for adding a _p_InitialAssignment object to an SBML model
-#' 
-#' Creates or copies a _p_InitialAssignment object and adds it to the _p_Model object
-#' 
-#' @title Add _p_InitialAssignment object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createInitialAssignment-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createInitialAssignment,_p_Model,ANY-method
 #' @aliases createInitialAssignment,_p_Model-method
-#' @name createInitialAssignment
-#' @note Where a new _p_InitialAssignment object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_InitialAssignment object to 'typical' defaults
 setMethod("createInitialAssignment", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("InitialAssignment", obj, i)
 )
 
-
-
-#' Function for adding a _p_Constraint object to an SBML model
-#' 
-#' Creates or copies a _p_Constraint object and adds it to the _p_Model object
-#' 
-#' @title Add _p_Constraint object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createConstraint-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createConstraint,_p_Model,ANY-method
 #' @aliases createConstraint,_p_Model-method
-#' @name createConstraint
-#' @note Where a new _p_Constraint object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_Constraint object to 'typical' defaults
 setMethod("createConstraint", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("Constraint", obj, i)
 )
 
-
-
-#' Function for adding a _p_Reaction object to an SBML model
-#' 
-#' Creates or copies a _p_Reaction object and adds it to the _p_Model object
-#' 
-#' @title Add _p_Reaction object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createReaction-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createReaction,_p_Model,ANY-method
 #' @aliases createReaction,_p_Model-method
-#' @name createReaction
-#' @note Where a new _p_Reaction object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_Reaction object to 'typical' defaults
 setMethod("createReaction", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("Reaction", obj, i)
 )
 
-
-
-#' Function for adding a _p_Event object to an SBML model
-#' 
-#' Creates or copies a _p_Event object and adds it to the _p_Model object
-#' 
-#' @title Add _p_Event object to a model
-#' @param object a _p_Model object
-#' @param i a vector of integers indicating objects to be copied or created
-#' @docType methods
-#' @rdname createEvent-methods
-#' @export 
+#' @rdname createFunctionDefinition-methods
 #' @aliases createEvent,_p_Model,ANY-method
 #' @aliases createEvent,_p_Model-method
-#' @name createEvent
-#' @note Where a new _p_Event object is created, the SBML Level and Version of
-#' the _p_Model object, (and any SBML package namespaces), are used to initialise
-#' the attributes of the new _p_Event object to 'typical' defaults
 setMethod("createEvent", signature(obj = "_p_Model", i = "ANY"),
           function(obj, i) add_to_model("Event", obj, i)
 )
 
 
 
-#' A function to add an SBML component object to a model
-#' 
-#' Can add a new object (initialised to typical defaults) or a copy of an existing
-#' object within the model
-#' 
-#' @title Add to a model
-#' @rdname add_to_model
-#' @name add_to_model
-#' @param compStr a string indicating the SBML component to be created 
-#' @param obj the model object to which the component will be added
-#' @param index a vector of integers indicating the object to be copied
-#' @note if \code{index} is missing or equal to zero, a new object (initialised to
-#' typical defaults) will be added to the model 
+###########################
+#Non exported functions
+
+
+## Can add a new object (initialised to typical defaults) or a copy of an existing
+## object within the model
+## @param compStr a string indicating the SBML component to be created 
+## @param obj the model object to which the component will be added
+## @param index a vector of integers indicating the object to be copied
+## @note if \code{index} is missing or equal to zero, a new object (initialised to
+## typical defaults) will be added to the model 
 add_to_model = function(compStr, obj, index) {
   
   if(missing(index) || index==0) {
@@ -418,16 +179,9 @@ add_to_model = function(compStr, obj, index) {
 }
 
 
-
-#' A function to add a new SBML component object to a model
-#' 
-#' The new object will be initialised to typical defaults
-#' 
-#' @title Add a new object to a model
-#' @rdname add_new_object
-#' @name add_new_object
-#' @param crStr a string indicating the SBML component to be created
-#' @param obj the model object to which the component will be added
+## @name add_new_object
+## @param crStr a string indicating the SBML component to be created
+## @param obj the model object to which the component will be added
 add_new_object = function(crStr, obj) {
   fun = paste0("Model_", crStr)
   do.call(fun, list(obj))  
@@ -435,15 +189,11 @@ add_new_object = function(crStr, obj) {
 
 
 
-#' A function to add a copy of an SBML component object to a model
-#' 
-#' @title Add a copy of an object to a model
-#' @rdname add_copy_object
-#' @name add_copy_object
-#' @param compStr a string indicating the SBML component to be copied
-#' @param obj the model object to which the component will be added
-#' @param index a vector of integers indicating the positions of the
-#' objects to be copied
+## A function to add a copy of an SBML component object to a model
+## @param compStr a string indicating the SBML component to be copied
+## @param obj the model object to which the component will be added
+## @param index a vector of integers indicating the positions of the
+## objects to be copied
 add_copy_object = function(compStr, obj, index) {
   # get list of copies of object(s) at positions given in 'index'
   fun = paste0("Model_get", compStr)
