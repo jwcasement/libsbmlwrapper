fbd2f004e0e4b7990eaabaabb5e8504a93a456f1
John Casement <j.w.casement@ncl.ac.uk>
2013-09-01 09:02:00 +0100

    Update edit.R

--- package/R/edit.R
+++ package/R/edit.R
@@ -1,17 +1,6 @@
-#' Edit an attribute of a _p_Component object
-#' 
-#' Changes the value of attribute 
-#' 
-#' @title Edit a _p_Component object attribute
-#' @param x a _p_Component object
-#' @param i a character string indicating the attribute to be changed
-#' @param value the new value of the attribute
-#' @docType methods
-#' @rdname edit-methods
-#' @export
+#' @rdname select-methods-_p_Model
 #' @aliases [[<-,_p_Component,ANY-method
 #' @aliases [[<-,_p_Component,character,missing-method
-#' @name edit_component
 setMethod("[[<-", signature(x = "_p_Component", i = "character", j = "missing", value = "ANY"),
           function(x, i, ..., value) {
             compStr = get_model_nodes()[match(class(x), get_component_classes())]            
@@ -20,19 +9,18 @@ setMethod("[[<-", signature(x = "_p_Component", i = "character", j = "missing", 
 )
 
 
+#########################################################################
+##Edit attributes
+## @param compStr a string containing the name of an SBML component (singular) 
+## @param obj the component object to be edited
+## @param attrStr a string containing the name of the attribute to be edited
+## @param newAttr the new value of the attribute in \code{attrStr} 
+## @return the modified component object \code{obj}
+##############################################################
 
-#' A function for editing an attribute of a component object
-#' 
-#' @title Edit an attribute
-#' @rdname edit_attribute
-#' @name edit_attribute
-#' @param compStr a string containing the name of an SBML component (singular) 
-#' @param obj the component object to be edited
-#' @param attrStr a string containing the name of the attribute to be edited
-#' @param newAttr the new value of the attribute in \code{attrStr} 
-#' @return the modified component object \code{obj}
+
 edit_attribute = function(compStr, obj, attrStr, newAttr) {
-  
+
   if(is.null(newAttr)) {
     newAttr = get_default_attribute(compStr, attrStr)   
   }
@@ -49,15 +37,8 @@ edit_attribute = function(compStr, obj, attrStr, newAttr) {
 
 
 
-#' A function to determine the typical default value of an attribute
-#' 
-#' @title Default attribute
-#' @rdname get_default_attribute
-#' @name get_default_attribute
-#' @param compStr a string containing the name of an SBML component (singular)
-#' @param attrStr a string containing the name of the attribute 
-#' @return the typical default value for the attribute in \code{attrStr}
-#' @note g is a global list returned by \code{set_and_get_defaults()} 
+## @return the typical default value for the attribute in \code{attrStr}
+## @note g is a global list returned by \code{set_and_get_defaults()} 
 get_default_attribute = function(compStr, attrStr) {
   return(get("defs", pkg_globals)$get_Defaults(compStr, attrStr))
 }
