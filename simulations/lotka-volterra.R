4862e1a6e2fe405a3a97eecd57950bd07db24e36
John Casement <j.w.casement@ncl.ac.uk>
2013-09-04 19:57:35 +0100

    Create lotka-volterra.R

---
+++ simulations/lotka-volterra.R
@@ -0,0 +1,28 @@
+library(smfsb)
+set.seed(1)
+
+# get SBML document as an XML string
+xmlStr = paste(readLines("http://www.staff.ncl.ac.uk/d.j.wilkinson/smfsb/2e/ch06-lv.xml"),
+               collapse = "")
+
+# get model from document
+doc = readSBMLFromString(xmlStr)
+mod = SBMLDocument_getModel(doc)
+
+# now obtain a realisation
+N = get_SPN_list(mod)
+out = gillespie(N,10000)
+
+## example code to produce an attractive plot of the realisation
+# plot prey
+plot(stepfun(out$t, out$x[,1]), main = "Stochastic LV process",
+     panel.first = grid(),
+     xlab = "Time", ylab = "Population", lwd = 2,
+     xlim = c(0, max(out$t)), ylim = c(0, 500))
+
+# plot predator
+lines(stepfun(out$t, out$x[,2]), lwd = 2, col = "slateblue")
+
+# add labels
+text(10, 30, "Prey", font = 2)
+text(17, 400, "Predator", font = 2, col = "slateblue")
